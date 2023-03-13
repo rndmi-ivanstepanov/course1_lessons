fa8ddd1c6198d0be20941fcead52faf847b4842f
Rndmi <SireOfAll79@yandex.ru>
2023-03-13 19:41:59 +0300

    13 / 03

---
+++ src/HomeWork13March/Author.java
@@ -0,0 +1,19 @@
+package HomeWork13March;
+
+public class Author {
+    private String firstName;
+    private String surname;
+
+    public Author(String firstName, String surname) {
+        this.firstName = firstName;
+        this.surname = surname;
+    }
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public String getSurname() {
+        return surname;
+    }
+}
